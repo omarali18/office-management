37132cae73cce786ddbe0db746ac404209982812
omarali18 <omar.eu18@gmail.com>
2026-01-13 17:25:28 +0600

    create order service

---
+++ src/main/java/com/project/officeManagement/service/OrderService.java
@@ -0,0 +1,45 @@
+package com.project.officeManagement.service;
+
+import com.project.officeManagement.dto.OrderRequestDto;
+import com.project.officeManagement.entity.OrderEntity;
+import com.project.officeManagement.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+//    private final PaymentService paymentService;
+
+    @Transactional(
+            propagation = Propagation.REQUIRED,
+            isolation = Isolation.READ_COMMITTED,
+            rollbackFor = Exception.class,
+            timeout = 30
+    )
+    public OrderEntity createOrder(OrderRequestDto dto) {
+
+        OrderEntity order = OrderEntity.builder()
+                .productName(dto.getProductName())
+                .amount(dto.getAmount())
+                .status("CREATED")
+                .build();
+
+        return orderRepository.save(order);
+    }
+
+    @Transactional
+    public OrderEntity markOrderPaid(Long orderId) {
+        OrderEntity order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new RuntimeException("Order not found"));
+
+        order.setStatus("PAID");
+        return orderRepository.save(order);
+    }
+}
+
