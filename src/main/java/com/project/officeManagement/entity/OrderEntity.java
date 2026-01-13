5433a02006b34ac0c40e04522066c296cd2aa8e8
omarali18 <omar.eu18@gmail.com>
2026-01-13 16:55:12 +0600

    Orderds and payment Emtity file create

---
+++ src/main/java/com/project/officeManagement/entity/OrderEntity.java
@@ -0,0 +1,25 @@
+package com.project.officeManagement.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "orders")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String productName;
+    private Double amount;
+    private String status; // CREATED, PAID
+}
+
---
+++ src/main/java/com/project/officeManagement/entity/PaymentEntity.java
@@ -0,0 +1,24 @@
+package com.project.officeManagement.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "payments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PaymentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long orderId;
+    private Double paidAmount;
+    private String paymentStatus; // SUCCESS, FAILED
+
+}
