cc0dec790978013afab19928518d9f79a7d0912e
omarali18 <omar.eu18@gmail.com>
2026-01-20 10:26:47 +0600

    user entity added for OneToOne

---
+++ src/main/java/com/project/officeManagement/entity/UserEntity.java
@@ -0,0 +1,21 @@
+package com.project.officeManagement.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+}
